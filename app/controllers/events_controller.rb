3ab246150cc0b8d60787e561edcfdffff133bfb0
Sarvagya Vaish <svaish3@survy-ubuntu.(none)>
2012-09-21 01:41:45 -0400

    trivial

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -60,7 +60,7 @@ class EventsController < ApplicationController
   # GET /events/1.json
   def show
     @event = Event.find(params[:id])
-    @members = @event.members
+    @members = @event.members.order("first_name")
 
     respond_to do |format|
       format.html # show.html.erb
