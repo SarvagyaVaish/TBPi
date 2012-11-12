13f2c37ccead1a7eab31e4e49b4cc139efc924fe
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-11-12 18:45:08 -0500

    now displays full name while searching by name

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   
   autocomplete :member, :gtid
-  autocomplete :member, :first_name
+  autocomplete :member, :first_name, :display_value => :fullName
   # before_filter RubyCAS::Filter
   load_and_authorize_resource
 
